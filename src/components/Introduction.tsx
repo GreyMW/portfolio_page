16ed93f49166df613b12237cb6eb67607e605ddf
GreyMW <GreyMW@gmail.com>
2023-07-16 22:13:24 -0500

    Welcome text ornament added

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -16,13 +16,19 @@ export default function Introduction() {
                             <h1 className={'welcome-text p-10'}>Welcome.</h1>
                         </div>
                         <div>
-                            <h1 className={'dev-text p-18'}>After being a Crime Analyst for seven years, I reminisced and found
-                                that what I enjoyed most about the job was the programming.
-                                Now I'm pursuing that passion.
-                                <br/><br/>
-                                Take a look through some of my projects. If they spark your interest, we should talk.
+                            <div className={"flex"}>
+                                <div className={"text-ornament"}>
+                                    <div className={"text-ornament-inner"}></div>
+                                </div>
+                                <h1 className={'dev-text'}>After being a Crime Analyst for seven years, I reminisced and found
+                                    that what I enjoyed most about the job was the programming.
+                                    Now I'm pursuing that passion.
+                                    <br/><br/>
+                                    Take a look through some of my projects. If they spark your interest, we should talk.
 
-                            </h1>
+                                </h1>
+                            </div>
+
                             <h1 className={'dev-text signature'}><br/>Graeme Weiss</h1>
 
                         </div>
