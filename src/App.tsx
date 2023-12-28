f013312d9547c84b3fe6d62b7fd2cb433466df50
GreyMW <GreyMW@gmail.com>
2023-12-28 04:57:09 -0600

    Setting up netlify redirects

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './styles/App.css'
 import Introduction from "./components/Introduction.tsx";
-import Navbar from "./components/Navbar.tsx";
+// import Navbar from "./components/Navbar.tsx";
 
 function App() {
 
     return (
         <div className={"relative"}>
-            <Navbar/>
+            {/*<Navbar/>*/}
             <Introduction/>
         </div>
     )
